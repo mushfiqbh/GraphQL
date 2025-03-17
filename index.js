ea10e300a83e245e9f08ea51396278386800595a
mushfiqbh <mushfiqbh@gmail.com>
2025-03-18 00:45:32 +0600

    Basic Completed

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ import { typeDefs } from "./schema.js";
 
 // resolvers
 const resolvers = {
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.game_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((a) => a.id === parent.author_id);
+    },
+    game(parent) {
+      return db.games.find((g) => g.id === parent.game_id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.author_id === parent.id);
+    },
+  },
   Query: {
     games() {
       return db.games;
@@ -29,22 +47,28 @@ const resolvers = {
       return db.reviews.find((review) => review.id === args.id);
     },
   },
-  Game: {
-    reviews(parent) {
-      return db.reviews.filter((r) => r.game_id === parent.id);
+  Mutation: {
+    deleteGame(_, args) {
+      db.games = db.games.filter((g) => g.id !== args.id);
+      return db.games;
     },
-  },
-  Review: {
-    author(parent) {
-      return db.authors.find((a) => a.id === parent.author_id);
+    addGame(_, args) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 10000).toString(),
+      };
+      db.games.push(game);
+      return game;
     },
-    game(parent) {
-      return db.games.find((g) => g.id === parent.game_id);
-    },
-  },
-  Author: {
-    reviews(parent) {
-      return db.reviews.filter((r) => r.author_id === parent.id);
+    updateGame(_, args) {
+      db.games = db.games.map((g) => {
+        if (g.id === args.id) {
+          return { ...g, ...args.edits };
+        }
+        return g;
+      });
+
+      return db.games.find((g) => g.id === args.id);
     },
   },
 };
